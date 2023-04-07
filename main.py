2f560f63303cc58868e08be3ca941113af13603f
Anastasia <nastyv15@mail.ru>
2023-04-07 22:07:10 +0300

    add homework python

---
+++ main.py
@@ -0,0 +1,82 @@
+# n = 8
+# print (n)
+
+# n = 'fddf'
+# print (n)
+
+# n = 5
+# print (type(n))
+
+# n = 'ddd'
+# print (type(n))
+
+# n = 'dd\'dd'
+# print (n)
+
+# n = 'dd"ffff"dd'
+# print (n)
+
+# print(5)
+# print(5)
+# # print(5, 8)
+# print(5)
+
+# """
+# print(5)
+# print(5)
+# print(5)
+# """
+
+# # print(5)
+# # print(5)
+# # print(5, 8)
+# print(5)
+
+# a = 5
+# b = 5.78
+# c = 'hello'
+# print  (f"{a} - {b} - {c}")
+# print  ("{} - {} - {}".format(a, b, c))
+
+# print('введите число:')
+# a = input()
+# b = input('введите второе число:')
+# print (a, '+' , b, '=' , a + b)
+
+# print('введите число:')
+# a = int (input())
+# b = int (input('введите второе число:'))
+# print (a, '+' , b, '=' , a + b)
+
+# c = 5.89
+# print(c)
+# n = int(c)
+# print(n)
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = int(c)
+# print(c)
+# print(type(c))
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# a = 3.4566453
+# b = 5.23454545
+# print(round(a * b, 2 )) #round - кол-во симовлов после запятой, "2" - кол-во символов
+
+# iter =  2
+# iter +=3 # iter = iter + 3
+# iter -=4 # iter = iter - 4 
+# iter *=3 # iter = iter * 3
+# iter /=3 # iter = iter / 3
+# iter //=3 # iter = iter // 3
+# iter %=3 # iter = iter % 3
+# iter **=3 # iter = iter ** 3
+
